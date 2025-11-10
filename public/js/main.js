f02d0527b43222932874ad348acba464006a39f5
Pedro A. Ramos <67925461+PedroA-78@users.noreply.github.com>
2025-11-10 13:25:22 -0300

    feat: final adjustments
    - menu hamburger foi implementado em telas menores
    - agora todos os links estão funcionais e redirecionando para suas respectivas páginas
    - alguns bugs visuais foram corrigidos

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,30 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('.header')
+    const menu = document.querySelector(".header_menu")
+    const nav = document.querySelector(".header_nav")
 
-window.addEventListener("scroll", () => {
-    const header = document.querySelector(".header")
-    
-    if (window.scrollY > 50) {
-        header.classList.add('scrolled')
-    } else {
-        header.classList.remove('scrolled')
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 50) {
+            header.classList.add('scrolled')
+        } else {
+            header.classList.remove('scrolled')
+        }
+    })
+
+    const menuLinks = [
+        menu,
+        ...document.querySelectorAll(".header_nav_link"),
+        document.querySelector('.header_cta')
+    ]
+
+    const interaction = () => {
+        if (getComputedStyle(menu).getPropertyValue('display') == "flex") {
+            header.classList.toggle('active')
+            nav.classList.toggle('active')
+        }
     }
+
+    menuLinks.forEach(link => {
+        link?.addEventListener('click', interaction)
+    })
 })
